e916319a304210ba7372199087ae8ec313579663
MolnarNatalia <LNA230478@bigmir.net>
2020-10-03 10:47:18 +0300

    init: start arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return 0;
+}
+let min = array[0];
+for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+        min = array[i];
+    }
+}
+return min;
+}
+exports.max = function max(array) {
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
